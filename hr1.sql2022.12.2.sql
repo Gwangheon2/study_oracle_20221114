d141065d58ab9687bfef7a1c015c8f103596445d
Gwangheon2 <rhkdgjsdl@naver.com>
2022-12-02 17:58:28 +0900

    2022-12-2 7장 8장

---
+++ hr1.sql2022.12.2.sql
@@ -0,0 +1,44 @@
+select*
+from    products;
+
+select*
+from    shipments;
+
+select*
+from    customers;
+
+
+select*
+from    stores;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
